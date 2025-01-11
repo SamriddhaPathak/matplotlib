73dbc674cabe4ab8e0ecd422bd3a916a30d611a8
Samriddha Pathak <samriddhapathak123333@gmail.com>
2025-01-11 23:25:00 +0545

    Update starting_code.py

--- Histograms/starting_code.py
+++ Histograms/starting_code.py
@@ -9,14 +9,14 @@ ages = [18, 19, 21, 25, 26, 26, 30, 32, 38, 45, 55]
 # ids = data['Responder_id']
 # ages = data['Age']
 
-# median_age = 29
+# median_age = 29    
 # color = '#fc4f30'
 
 # plt.legend()
 
 plt.title('Ages of Respondents')
 plt.xlabel('Ages')
-plt.ylabel('Total Respondents')
+plt.ylabel('Total Respondents')  
 
 plt.tight_layout()
 
